81f7f0641d071b637c8c07877d8c36e52fd546a5
sousa-p <p.soousa7@gmail.com>
2023-11-27 23:36:38 -0300

    Feature views e imagens

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
+import 'package:conversor_moedas/views/home_view.dart';
+import 'package:flutter/material.dart';
 
+main() {
+  runApp(const AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const HomeView(),
+    );
+  }
+}

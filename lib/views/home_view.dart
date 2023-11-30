c1aa92b1914563c36d72446b0993214eb9c1901d
sousa-p <p.soousa7@gmail.com>
2023-11-30 14:16:09 -0300

    Fix problema de scroll

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -21,51 +21,52 @@ class _HomeViewState extends State<HomeView> {
     homeController = HomeController(toText, fromText);
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SizedBox(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      child: Padding(
-        padding: const EdgeInsets.only(top: 100, right: 25, left: 25),
-        child: Column(
-          children: [
-            Image.asset('assets/logo.png', width: 250),
-            CurrencyBox(
-              selectedItem: homeController.toCurrency,
-              items: homeController.currencies,
-              onChanged: (model) {
-                setState(() {
-                  homeController.toCurrency = model!;
-                });
-              },
-              controller: toText,
-            ),
-            const SizedBox(height: 16),
-            CurrencyBox(
-              selectedItem: homeController.fromCurrency,
-              items: homeController.currencies,
-              onChanged: (model) {
-                setState(() {
-                  homeController.fromCurrency = model!;
-                });
-              },
-              controller: fromText,
-            ),
-            const SizedBox(
-              height: 64,
-            ),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 50, vertical: 25)),
-                onPressed: () {
-                  homeController.convert();
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 100, right: 25, left: 25),
+          child: Column(
+            children: [
+              Image.asset('assets/logo.png', width: 250),
+              CurrencyBox(
+                selectedItem: homeController.toCurrency,
+                items: homeController.currencies,
+                onChanged: (model) {
+                  setState(() {
+                    homeController.toCurrency = model!;
+                  });
                 },
-                child: const Text('Converter'))
-          ],
+                controller: toText,
+              ),
+              const SizedBox(height: 16),
+              CurrencyBox(
+                selectedItem: homeController.fromCurrency,
+                items: homeController.currencies,
+                onChanged: (model) {
+                  setState(() {
+                    homeController.fromCurrency = model!;
+                  });
+                },
+                controller: fromText,
+              ),
+              const SizedBox(
+                height: 64,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 50, vertical: 25)),
+                  onPressed: () {
+                    homeController.convert();
+                  },
+                  child: const Text('Converter'))
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
